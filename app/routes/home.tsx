fdedeeead962fdb8d3334d79217c9d50c6e10fc2
Supreet0 <supreetpatel2004@gmail.com>
2026-05-13 12:52:39 +0530

    feat: initialize project with React Router and Tailwind CSS
    - Add package.json with dependencies for React, React Router, and Tailwind CSS.
    - Include favicon.ico for the application.
    - Create react-router.config.ts for server-side rendering configuration.
    - Set up TypeScript configuration in tsconfig.json with strict settings.
    - Define AuthState and AuthContext interfaces in type.d.ts for authentication management.
    - Configure Vite with React Router and Tailwind CSS plugins in vite.config.ts.

---
+++ app/routes/home.tsx
@@ -0,0 +1,106 @@
+import { ArrowRight, ArrowUp, Clock, Layers } from "lucide-react";
+import Navbar from "../../components/Navbar";
+import type { Route } from "./+types/home";
+import Button from "../../components/ui/Button";
+import Upload from "../../components/Upload";
+import { useNavigate } from "react-router";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "Roomify - Design Beautiful Spaces" },
+    {
+      name: "description",
+      content: "Design beautiful spaces at the speed of thought with Roomify",
+    },
+  ];
+}
+
+export default function Home() {
+  const navigate = useNavigate();
+  const handleUploadComplete = async (base64Image: string) => {
+    const newId = Date.now().toString();
+    navigate(`/visualizer/${newId}`);
+    return true;
+  };
+
+  return (
+    <div className="home">
+      <Navbar />
+      <section className="hero">
+        <div className="announce">
+          <div className="dot">
+            <div className="pulse"></div>
+          </div>
+          <p>Introducing Roomify 1.0</p>
+        </div>
+        <h1>Build beautiful spaces at the speed of thought with Roomify</h1>
+        <p className="subtitle">
+          Roomify is an AI-first design platform that helps you create stunning
+          interiors in no time.
+        </p>
+        <div className="actions">
+          <a href="/upload" className="cta">
+            Start Designing <ArrowRight className="icon" />
+          </a>
+          <Button variant="outline" size="lg" className="demo">
+            Learn More
+          </Button>
+        </div>
+        <div id="upload" className="upload-shell">
+          <div className="grid-overlay" />
+          <div className="upload-card">
+            <div className="upload-head">
+              <div className="upload-icon">
+                <Layers className="icon" />
+              </div>
+
+              <h3>Upload your floor plan</h3>
+              <p>Supports JPG, PNG, formats up to 10MB</p>
+            </div>
+            <Upload onComplete={handleUploadComplete} />
+          </div>
+        </div>
+      </section>
+
+      <section className="projects">
+        <div className="section-inner">
+          <div className="section-head">
+            <div className="copy">
+              <h2>Projects</h2>
+              <p>
+                Your latest work and shared community projects, all in one
+                place.
+              </p>
+            </div>
+          </div>
+          <div className="projects-grid">
+            <div className="project-card group">
+              <div className="preview">
+                <img
+                  src="https://roomify-mlhuk267-dfwu1i.puter.site/projects/1770803585402/rendered.png"
+                  alt="Project Preview"
+                />
+                <div className="badge">
+                  <span>Community</span>
+                </div>
+              </div>
+              <div className="card-body">
+                <div>
+                  <h3>Project Title</h3>
+                  <div className="meta">
+                    <Clock size={12} />
+                    <span>{new Date("2023-10-01").toLocaleDateString()}</span>
+                    <span>By JS Mastery</span>
+                  </div>
+                </div>
+                <div className="arrow">
+                  <ArrowUp size={18} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
